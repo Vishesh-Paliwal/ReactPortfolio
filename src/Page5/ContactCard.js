325b57a4d1ac9ee131e0c76c8c20aba89d392960
Vishesh Paliwal <vishesh.23bcs10013@ms.sst.scaler.com>
2024-06-02 16:19:11 +0530

    page5 responsive

--- src/Page5/ContactCard.js
+++ src/Page5/ContactCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './conta.css';
 function Contcard() {
   return (
-    <div className="contact-card">
+    <div id='cd' className="contact-card">
       <input className="input-field" type="text" placeholder="Name" />
       <input className="input-field" type="email" placeholder="Email" />
       <input className="input-field" type="tel" placeholder="Phone Number" />
-      <textarea className="input-field" placeholder="Message"></textarea>
+      <textarea id='mess' className="input-field" placeholder="Message"></textarea>
       <button className="send-button">Send Message</button>
     </div>
   );
