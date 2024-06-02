7fba59d8f5938b28d76780737c70d303038c2da3
Vishesh Paliwal <vishesh.23bcs10013@ms.sst.scaler.com>
2024-06-02 12:57:08 +0530

    change title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Page1 from './Page1/Page1'
-// import About from './Page2/About'
 import Skill from './Page3/Skills'
 import Exper from './Page4/Experience'
 import Cont from './Page5/Contact'
