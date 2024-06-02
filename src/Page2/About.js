b9f056e4815a203acf033d8e598975b24b5e8296
Vishesh Paliwal <vishesh.23bcs10013@ms.sst.scaler.com>
2024-06-02 16:33:56 +0530

    removed draggable in mobile screen

--- src/Page2/About.js
+++ src/Page2/About.js
@@ -1,37 +1,9 @@
-// import Intrest from './IntrestCard'
-// import './About.css'
-// import Avatar from "../Assets/avatar.svg"
-
-// function about(){
-//     return <div>
-//         <div id='ab'>ABOUT</div>
-//         <div id='par'>
-//             <img src={Avatar} alt="self-img"></img>
-//             <div id='txt'>
-//             <p>I'm Vishesh, a passionate software developer. I specialize in Web Development, crafting seamless user experiences and robust backend solutions. With a knack for problem-solving, I thrive on creating elegant code that makes an impact. I'm always exploring new tools and techniques to stay at the forefront of the ever-evolving tech landscape.</p>
-//             <p>Open to collaborations and exciting projects, I'm eager to connect with fellow developers and tech enthusiasts. Let's build something amazing together!Happy coding!</p>   
-//             </div>
-//         </div>
-//         <h2>Intrests :</h2>
-//         <div id='AllCards'>
-//             <Intrest text="Software Engenering"/>
-//             <Intrest text="Web-Development"/>
-//             <Intrest text="Competitive Proagraming"/>
-//             <Intrest text="Web Scraping"/>
-//         </div>
-//     </div>
-// }
-
-// export default about;
-
-
-
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import Intrest from './IntrestCard';
 import './About.css';
 import Avatar from "../Assets/avatar.svg";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function About() {
     const controls = useAnimation();
@@ -39,10 +11,21 @@ function About() {
         threshold: 0.1,
     });
 
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+
     useEffect(() => {
         if (inView) {
             controls.start('visible');
         }
+
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 600);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, [controls, inView]);
 
     const variants = {
@@ -50,15 +33,22 @@ function About() {
         visible: { opacity: 1, y: 0, transition: { duration: 2 } },
     };
 
+    const dragConstraints = {
+        top: -500,
+        left: 0,
+        right: 500,
+        bottom: 0,
+    };
+
     return (
         <div>
             <div id='ab'>ABOUT</div>
             <div id='par'>
-            <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            ></motion.div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    viewport={{ once: true }}
+                ></motion.div>
                 <img id='profileimg' src={Avatar} alt="self-img" />
                 <div id='txt'>
                     <motion.p
@@ -80,66 +70,46 @@ function About() {
                 </div>
             </div>
             <h2>Intrests :</h2>
-            
             <div id='AllCards'>
-            <motion.div
-                drag
-                dragConstraints={{
-                top: -500,
-                left: 0,
-                right: 500,
-                bottom: 0,
-                }}
-            ><Intrest text="Software Engineering" /></motion.div>
                 <motion.div
-                drag
-                dragConstraints={{
-                    top: -500,
-                    left: 0,
-                    right: 500,
-                    bottom: 0,
-                }}
-            ><Intrest text="Web-Development" /></motion.div>
+                    drag={!isMobile}
+                    dragConstraints={dragConstraints}
+                >
+                    <Intrest text="Software Engineering" />
+                </motion.div>
                 <motion.div
-                drag
-                dragConstraints={{
-                    top: -500,
-                    left: 0,
-                    right: 500,
-                    bottom: 0,
-                }}
-            ><Intrest text="Competitive Proagramming" /></motion.div>
+                    drag={!isMobile}
+                    dragConstraints={dragConstraints}
+                >
+                    <Intrest text="Web-Development" />
+                </motion.div>
                 <motion.div
-                drag
-                dragConstraints={{
-                    top: -500,
-                    left: 0,
-                    right: 500,
-                    bottom: 0,
-                }}
-            ><Intrest text="Web-Scraping" /></motion.div>
-              <motion.div
-                drag
-                dragConstraints={{
-                    top: -500,
-                    left: 0,
-                    right: 500,
-                    bottom: 0,
-                }}
-            ><Intrest text="Data Analysis" /></motion.div>
-              <motion.div
-                drag
-                dragConstraints={{
-                    top: -500,
-                    left: 0,
-                    right: 500,
-                    bottom: 0,
-                }}
-            ><Intrest text="Visualization" /></motion.div>
+                    drag={!isMobile}
+                    dragConstraints={dragConstraints}
+                >
+                    <Intrest text="Competitive Programming" />
+                </motion.div>
+                <motion.div
+                    drag={!isMobile}
+                    dragConstraints={dragConstraints}
+                >
+                    <Intrest text="Web-Scraping" />
+                </motion.div>
+                <motion.div
+                    drag={!isMobile}
+                    dragConstraints={dragConstraints}
+                >
+                    <Intrest text="Data Analysis" />
+                </motion.div>
+                <motion.div
+                    drag={!isMobile}
+                    dragConstraints={dragConstraints}
+                >
+                    <Intrest text="Visualization" />
+                </motion.div>
             </div>
         </div>
     );
 }
 
 export default About;
-
